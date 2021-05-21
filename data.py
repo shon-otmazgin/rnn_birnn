807eb55ed59349a8127d17ef4e0c894df56518cb
shon-otmazgin <shon711@gmail.com>
2021-05-21 15:52:49 +0300

    some cleanup

--- data.py
+++ data.py
@@ -41,51 +41,6 @@ def read_data(filename, with_labels):
         return X, unique_x_toks
 
 
-# class DatasetA(Dataset):
-#     def __init__(self, filename, return_y, tokens2ids=None, tags2ids=None):
-#         self.return_y = return_y
-#         data = read_data(filename, with_labels=return_y)
-#         if return_y:
-#             self.X, self.y, self.unique_x_toks, self.unique_y_toks = data
-#         else:
-#             self.X, self.unique_x_toks = data
-#
-#         if tokens2ids is None:
-#             self.tokens2ids = {t: i for i, t in enumerate(self.unique_x_toks)}
-#             self.tokens2ids[UNK] = len(self.tokens2ids)
-#             self.tokens2ids[PAD] = len(self.tokens2ids)
-#         else:
-#             self.tokens2ids = tokens2ids
-#
-#         if tags2ids is None and return_y:
-#             self.tags2ids = {t: i for i, t in enumerate(self.unique_y_toks)}
-#         else:
-#             self.tags2ids = tags2ids
-#
-#         self.vocab_size = len(self.tokens2ids.keys())
-#         self.tagset_size = len(self.tags2ids.keys())
-#
-#         self.X = [self._tensorize_x(x) for x in self.X]
-#         if return_y:
-#             self.y = [self._tensorize_y(y) for y in self.y]
-#
-#     def __len__(self):
-#         return len(self.X)
-#
-#     def __getitem__(self, item):
-#         if self.return_y:
-#             return self.X[item], self.y[item]
-#         return self.X[item]
-#
-#     def _tensorize_x(self, x):
-#         idxs = [self.tokens2ids[t] if t in self.tokens2ids else self.tokens2ids[UNK] for t in x]
-#         return torch.tensor(idxs, dtype=torch.long)
-#
-#     def _tensorize_y(self, y):
-#         idxs = [self.tags2ids[t] for t in y]
-#         return torch.tensor(idxs)
-
-
 class TagDataset(Dataset):
     def __init__(self, filename, return_y, tokens2ids=None, tags2ids=None):
         self.return_y = return_y
--- models.py
+++ models.py
@@ -3,36 +3,6 @@ from torch import nn
 from torch.nn.utils.rnn import pad_packed_sequence, pack_padded_sequence
 
 
-class BiLSTMTaggerA(nn.Module):
-    def __init__(self, vocab_size, tagset_size, padding_idx):
-        super(BiLSTMTaggerA, self).__init__()
-        self.vocab_size = vocab_size
-        self.tagset_size = tagset_size
-        self.char_emb_size = 50
-        self.token_emb_size = 300
-        self.lstm_c_dim = 300
-        self.lstm_dim = 768
-
-        self.word_emb = nn.Embedding(self.vocab_size, self.emb_size, padding_idx=padding_idx)
-        self.bilstm1 = nn.LSTM(bidirectional=True, input_size=self.emb_size,
-                               hidden_size=self.s_dim, num_layers=1, batch_first=True)
-        self.bilstm2 = nn.LSTM(bidirectional=True, input_size=2*self.s_dim,
-                               hidden_size=self.s_dim, num_layers=1, batch_first=True)
-
-        self.linear = nn.Linear(2*self.s_dim, tagset_size)
-
-    def forward(self, input_ids, input_lens):
-        embds = self.word_emb(input_ids)        # [batch, sequence_len, emb]
-
-        x_packed = pack_padded_sequence(embds, input_lens, batch_first=True, enforce_sorted=False)
-        lstm_out, _ = self.bilstm1(x_packed)          # [batch, sequence_len, out_dim]
-        lstm_out, _ = self.bilstm2(lstm_out)
-        output_padded, output_lengths = pad_packed_sequence(lstm_out, batch_first=True)
-
-        output = self.linear(output_padded)
-        return output
-
-
 class BiLSTMTagger(nn.Module):
     def __init__(self, vocab_size, alphabet_size, tagset_size, token_padding_idx, char_padding_idx, char_level, token_level):
         super(BiLSTMTagger, self).__init__()
