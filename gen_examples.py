9b7985538f4cde872ef87c38a7eae8f065af4984
shon-otmazgin <shon711@gmail.com>
2021-05-22 17:24:20 +0300

    generating new example and refactor gen_examples.py

--- gen_examples.py
+++ gen_examples.py
@@ -9,22 +9,31 @@ def gen_examples(reg, n, file):
         for i in range(n):
             s = []
             for g in reg:
-                stop = random.random()          # generate random prob to stop
+                stop = random.random()                                          # generate random prob to stop
                 while True:
-                    s.append(g if g != r'\d' else str(random.randint(1, 9)))
-                    if random.random() > stop:   # generate the same char
+                    s.append(g if g != r'\d' else str(random.randint(1, 9)))    # generate the same char
+                    if random.random() <= stop:                                 # stop in range [0, stop]
                         break
             f.write(''.join(s) + '\n')
 
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Language generator')
-    parser.add_argument("--n", type=int, default=0, help='Number of positive/negative examples (default: 0)')
-    parser.add_argument("--suffix_file_name", type=str, default='_examples', help='pos_suffix / neg_suffix')
+    parser.add_argument("N", type=int, default=500, help='Number of positive/negative examples (default: 0)')
+    parser.add_argument("--suffix_file_name", type=str, default='examples', help='pos_suffix / neg_suffix defualt: _examples')
     args = parser.parse_args()
 
+    n = args.N
+    pos_file = 'pos' + '_' + args.suffix_file_name
+    neg_file = 'neg' + '_' + args.suffix_file_name
     pos_reg = [r'\d', r'a', r'\d', r'b', r'\d', r'c', r'\d', r'd', r'\d']
     neg_reg = [r'\d', r'a', r'\d', r'c', r'\d', r'b', r'\d', r'd', r'\d']
 
-    gen_examples(pos_reg, args.n, 'pos' + '_' + args.suffix_file_name)
-    gen_examples(neg_reg, args.n, 'neg' + '_' + args.suffix_file_name)
+    print(f'Generating {n} positive examples to {pos_file}')
+    gen_examples(pos_reg, n, pos_file)
+    print('Done.')
+
+    print(f'Generating {n} negative examples to {neg_file}')
+    gen_examples(neg_reg, n, neg_file)
+    print('Done.')
+
